17d5f12eca8e0c5e30b9e364823018ee4e21d44d
MateusV.Gomes <63268362+MateusVGomes@users.noreply.github.com>
2022-04-08 09:27:25 -0300

    Add files via upload

---
+++ Conversor/script.js
@@ -0,0 +1,6 @@
+const valDolar=document.getElementById("valDolar");
+const valReal=document.getElementById("valReal");
+const valTax=document.getElementById("valTax");
+const btnDolar=document.getElementById("btnDolar");
+const btnReal=document.getElementById("btnReal");
+const btnlTax=document.getElementById("btnTax");
