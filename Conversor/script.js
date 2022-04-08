de449235ebcfc24d18794758085004cc306d1b84
MateusV.Gomes <63268362+MateusVGomes@users.noreply.github.com>
2022-04-08 11:05:36 -0300

    Add files via upload

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,6 +1,26 @@
+
 const valDolar=document.getElementById("valDolar");
 const valReal=document.getElementById("valReal");
 const valTax=document.getElementById("valTax");
-const btnDolar=document.getElementById("btnDolar");
-const btnReal=document.getElementById("btnReal");
-const btnlTax=document.getElementById("btnTax");
+
+
+document.getElementById("btnTax").disabled = false;
+document.getElementById("btnDolar").disabled = true;
+document.getElementById("btnReal").disabled =  true;
+
+function taxa(){
+if(valTaxa.value<=0 || valTaxa.isNaN) {
+
+    alert("Valor não inserio não é valido ou não foi inserido por favor insira algo maior que 0");
+    
+}
+else{
+    valTax=document.getElementById("valTax").value;
+    alert(valTaxa.value)
+    document.getElementById("btnTax").disabled = true;
+    document.getElementById("btnDolar").disabled = false;
+    document.getElementById("btnReal").disabled =  false;   
+}
+
+}
+
