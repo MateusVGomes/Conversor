22c4d364e37a81295c281d7c7cfd01a46030c68a
MateusV.Gomes <63268362+MateusVGomes@users.noreply.github.com>
2022-04-11 10:11:50 -0300

    Correção

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,7 +1,7 @@
 
-const valDolar=document.getElementById("valDolar");
-const valReal=document.getElementById("valReal");
-const valTax=document.getElementById("valTax");
+var valDolar=document.getElementById("inputDolar");
+var valReal=document.getElementById("inputReal");
+var valTax=document.getElementById("inputTaxa");
 
 
 document.getElementById("btnTax").disabled = false;
@@ -9,17 +9,19 @@ document.getElementById("btnDolar").disabled = true;
 document.getElementById("btnReal").disabled =  true;
 
 function taxa(){
-if(valTaxa.value<=0 || valTaxa.isNaN) {
+if(inputTaxa.value<=0 || inputTaxa.isNaN) {
 
     alert("Valor não inserio não é valido ou não foi inserido por favor insira algo maior que 0");
     
 }
 else{
-    valTax=document.getElementById("valTax").value;
-    alert(valTaxa.value)
+    var input=document.getElementById("inputTaxa").value;
+    valTax=input;
+    alert(valTax);
     document.getElementById("btnTax").disabled = true;
     document.getElementById("btnDolar").disabled = false;
     document.getElementById("btnReal").disabled =  false;   
+    
 }
 
 }
